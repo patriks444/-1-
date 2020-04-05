909dfff33e68989dab30ee2219775a785c5ec9e4
Николай <Patriks7@yandex.ru>
2020-04-05 18:29:22 +0300

    множества создание в слияние

--- list.py
+++ list.py
@@ -1,7 +1,13 @@
-my_box = {}
-my_box["слон"] = "элефант"
-print(my_box["слон"])
-my_box["машина"] = "форд"
-print(my_box["машина"])
-print("слон" in my_box)
-print(my_box.get("слон", "кот"))
+#my_box = {}
+#my_box["слон"] = "элефант"
+#print(my_box["слон"])
+#my_box["машина"] = "форд"
+#print(my_box["машина"])
+#print("слон" in my_box)
+#print(my_box.get("слон", "кот"))
+
+my_set = set([1,3,2,3,4,5,6,2,1,6,7,2,1,6])
+print(my_set)
+other_set = {1,4,5,7,8,3,2,3,5,7}
+print(other_set)
+print(my_set | other_set)
